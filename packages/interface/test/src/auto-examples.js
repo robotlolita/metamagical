2b7e5f71f42422bf0de246a0b54efa882c92e6aa
Quildreen Motta <queen@robotlolita.me>
2016-05-24 15:54:08 -0300

    test(interface): Runs documentation examples

---
+++ packages/interface/test/src/auto-examples.js
@@ -0,0 +1,16 @@
+//----------------------------------------------------------------------
+//
+// This source file is part of the Meta:Magical project.
+//
+// See LICENCE for licence information.
+// See CONTRIBUTORS for the list of contributors to the project.
+//
+//----------------------------------------------------------------------
+
+// This module runs all of the example-based tests defined in the
+// documentation for this module.
+const metamagical = require('../../');
+const defineTests = require('metamagical-mocha-bridge')(metamagical, describe, it);
+
+defineTests(metamagical);
+
